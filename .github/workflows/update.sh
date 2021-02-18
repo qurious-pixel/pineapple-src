24cd8719b261d75e8db538d968ada9770fc0a007
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-02-17 20:38:09 -0800

    Directly launch game
    Read game path even when updating

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -5,11 +5,13 @@ export QT_QPA_PLATFORMTHEME=gtk3
 zenity --question --timeout=10 --title="yuzu updater" --text="New update available. Update now?" --icon-name=yuzu --window-icon=yuzu.svg --height=80 --width=400
 answer=$?
 
+echo "GAMEPATH '${GAMEPATH}'"
+
 if [ "$answer" -eq 0 ]; then 
-	$APPDIR/usr/bin/AppImageUpdate $PWD/yuzu-x86_64.AppImage && $PWD/yuzu-x86_64.AppImage
+	$APPDIR/usr/bin/AppImageUpdate $PWD/yuzu-x86_64.AppImage && $PWD/yuzu-x86_64.AppImage "${GAMEPATH}"
 elif [ "$answer" -eq 1 ]; then
-	$APPDIR/usr/bin/yuzu
+	$APPDIR/usr/bin/yuzu "${GAMEPATH}"
 elif [ "$answer" -eq 5 ]; then
-	$APPDIR/usr/bin/yuzu
+	$APPDIR/usr/bin/yuzu "${GAMEPATH}"
 fi
 exit 0
